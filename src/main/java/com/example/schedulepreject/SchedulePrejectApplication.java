d5076c294f98bb198cea68fbeb5ae081ef45bf71
hyohee0613 <gygml9@gmail.com>
2025-05-22 18:49:56 +0900

    first commit

---
+++ src/main/java/com/example/schedulepreject/SchedulePrejectApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulepreject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePrejectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulePrejectApplication.class, args);
+    }
+
+}
