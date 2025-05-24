e1b21912051004ee1f619ea544d073ac3fb27333
hyohee0613 <gygml9@gmail.com>
2025-05-24 20:19:16 +0900

    Repository 인터페이스 작성 완료

---
+++ src/main/java/com/example/scheduleproject/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.scheduleproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+//Auditing 기능을 작동시키려면, 어떤 이벤트가 일어날 때 자동으로 반응하게 만들어야 한다
+//Auditing: 등록하거나 수정하면 알려줘 → 자동으로 updatedAt에 시간 기록할게. 이걸 실행해주는 게 AuditingEntityListener
+public abstract class BaseEntity {
+    //직접 인스턴스를 생성할 일이 없으므로 abstract로 선언, 오직 상속용으로만 쓴다
+
+    @LastModifiedDate
+    @Column(name = "updated_at")    //updatable = false 설정은 create_at에 쓰기 (생성 시간이 수정되지 못하게)
+    private LocalDateTime updatedAt;
+
+}
--- src/test/java/com/example/scheduleproject/SchedulePrejectApplicationTests.java
+++ src/test/java/com/example/scheduleproject/SchedulePrejectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.schedulepreject;
+package com.example.scheduleproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
